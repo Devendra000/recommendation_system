1324b59f575f3b84d994d9640e7a9e61d59bb510
Dezayn000 <devendra.hamal058@gmail.com>
2024-07-03 22:52:44 +0545

    recommendation syste

---
+++ recommend.py
@@ -0,0 +1,239 @@
+import pandas as pd
+import fastapi as FastApi
+import uvicorn
+import os
+from dotenv import load_dotenv
+import pymysql
+import sys
+import json
+
+from collections import defaultdict
+
+load_dotenv()
+
+
+from typing import Union
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+# get all product
+def getAllProducts():
+    connection = pymysql.connect(
+                    host=os.getenv('MYSQL_HOST'),
+                    user=os.getenv('MYSQL_USER'),
+                    password=os.getenv('MYSQL_PASSWORD'),                             
+                    db=os.getenv('MYSQL_DATABASE'),
+                    charset='utf8mb4',
+                    cursorclass=pymysql.cursors.DictCursor ) 
+
+    try:
+        with connection.cursor() as cursor:
+            sql = "SELECT p.id as id, p.name AS name, c.name AS category, education_level FROM products p JOIN categories c ON p.category_id = c.id;"
+            cursor.execute(sql)
+            products = cursor.fetchall()
+            products = pd.DataFrame(products, columns=['id', 'name', 'category', 'education_level'])
+            return products
+
+    finally:
+        connection.close()
+
+# get user history
+def userHistory(userId: int):
+    connection = pymysql.connect(host=os.getenv('MYSQL_HOST'),
+                                user=os.getenv('MYSQL_USER'),
+                                password=os.getenv('MYSQL_PASSWORD'),                             
+                                db=os.getenv('MYSQL_DATABASE'),
+                                charset='utf8mb4',
+                                cursorclass=pymysql.cursors.DictCursor) 
+
+    try:
+        with connection.cursor() as cursor: 
+            # SQL 
+            sql = "SELECT clicks FROM users where id = %s" 
+            # Execute query.
+            cursor.execute(sql,(userId))
+            
+            userCLicks = cursor.fetchall()
+            clicked_products = pd.DataFrame(userCLicks, columns=['clicks'])
+            return clicked_products
+
+    finally:
+        connection.close()
+
+# Define a hierarchical mapping of levels (adjust as needed)
+level_hierarchy = {
+    'Bachelors 1st Sem': 1,
+    'Bachelors 2nd Sem': 2,
+    # Add more levels as per your domain knowledge
+}
+
+products = getAllProducts()
+
+# Calculate combined similarity based on category and education_level
+def calculate_similarity(clicked_category, clicked_level, product_category, product_level):
+    category_similarity = 1.0 if clicked_category == product_category else 0.0
+    
+    level_similarity = 0.0
+    if clicked_level == product_level:
+        level_similarity = 1.0
+    elif level_hierarchy.get(clicked_level, 0) > level_hierarchy.get(product_level, 0):
+        level_similarity = 0.8
+    elif level_hierarchy.get(clicked_level, 0) < level_hierarchy.get(product_level, 0):
+        level_similarity = 0.6
+    
+    combined_similarity = 0.5 * category_similarity + 0.5 * level_similarity
+    return combined_similarity
+
+# Recommend products based on user's clicked history
+def recommend_products_with_user_history(user_clicked_items, n):
+    print('in rec pro')
+    recommended_products = []
+    for product_id in user_clicked_items:
+        
+        # Index of the clicked product in the products DataFrame
+        idx = products.index[products['id'] == product_id].tolist()[0]
+        
+        clicked_name = products.loc[idx, 'name']
+        clicked_category = products.loc[idx, 'category']
+        clicked_level = products.loc[idx, 'education_level']
+        
+        # Calculate similarity scores with all products based on category and education_level
+        similarity_scores = []
+        for i in range(len(products)):
+            if products.loc[i, 'id'] != product_id:
+                
+                product_category = products.loc[i, 'category']
+                product_level = products.loc[i, 'education_level']
+                similarity_score = calculate_similarity(clicked_category, clicked_level, product_category, product_level)
+                similarity_scores.append((i, similarity_score))
+        
+        # Sort by similarity score (descending)
+        similarity_scores_sorted = sorted(similarity_scores, key=lambda x: x[1], reverse=True)
+        
+        # Extract top n recommendations
+        top_n_recommendations = []
+        for i, score in similarity_scores_sorted[:n]:
+            top_n_recommendations.append({
+                'id': products.loc[i, 'id'],
+                'name': products.loc[i, 'name'],
+                'similarity_score': score,
+            })
+        
+        recommended_products.append({
+            'clicked_product': clicked_name,
+            'recommended_products': top_n_recommendations
+        })
+        
+    return recommended_products
+
+
+# Recommend products for clicked product
+def recommend_products_with_productId(product_id, n):
+    print('in recommend_products_with_productId')
+    recommended_products = []
+    
+    print(products)
+    # Index of the clicked product in the products DataFrame
+    idx = products.index[products['id'] == product_id].tolist()[0]
+    clicked_name = products.loc[idx, 'name']
+    clicked_category = products.loc[idx, 'category']
+    clicked_level = products.loc[idx, 'education_level']
+    
+    # Calculate similarity scores with all products based on category and education_level
+    similarity_scores = []
+    for i in range(len(products)):
+        print ("product id", products.loc[i, 'id'])
+        if products.loc[i, 'id'] != product_id:
+            product_category = products.loc[i, 'category']
+            product_level = products.loc[i, 'education_level']
+            similarity_score = calculate_similarity(clicked_category, clicked_level, product_category, product_level)
+            print('prod',similarity_score)
+            similarity_scores.append((i, similarity_score))
+    
+        print (similarity_scores)
+        # Sort by similarity score (descending)
+        similarity_scores_sorted = sorted(similarity_scores, key=lambda x: x[1], reverse=True)
+        
+        # Extract top n recommendations
+        top_n_recommendations = []
+        for i, score in similarity_scores_sorted[:n]:
+            top_n_recommendations.append({
+                'id': products.loc[i, 'id'],
+                'name': products.loc[i, 'name'],
+                'similarity_score': score,
+            })
+        
+        recommended_products.append({
+            'clicked_product': clicked_name,
+            'recommended_products': top_n_recommendations
+        })
+        
+    return recommended_products
+    
+# Recommend products based on user's clicked history
+@app.get("/recommendItemsForUser/{userId}/{n}")
+def getRecommendation(userId: int, n: int):
+    # return id
+    clicked_products = userHistory(userId)
+    recommendations = recommend_products_with_user_history(json.loads(clicked_products['clicks'][0]), n)
+    # return recommendations
+    recommendation = []
+    for rec in recommendations:
+        clicked = rec['clicked_product']
+        for product in enumerate(rec['recommended_products']):
+            recommendation.append({'id':product[1]['id'].item(), 'name':product[1]['name'], 'reason':f'You clicked on {clicked}', 'similarity_score':product[1]['similarity_score']})
+            
+    for rec in recommendation:
+        rec['id'] = int(rec['id'])
+
+    # Sort recommendations based on similarity_score in descending order
+    sorted_recommendations = sorted(recommendation, key=lambda x: x['similarity_score'], reverse=True)
+
+    max_similarity = {}
+    for rec in sorted_recommendations:
+        id_ = rec['id']
+        score = rec['similarity_score']
+        if id_ not in max_similarity or score > max_similarity[id_]['similarity_score']:
+            max_similarity[id_] = rec
+
+    filtered_recommendations = list(max_similarity.values())
+
+    return filtered_recommendations[:n]
+
+# Recommend products for clicked product
+@app.get("/recommendItemsByProductId/{product_id}/{n}")
+def getRecommendation(product_id: int, n: int):
+    recommendations = recommend_products_with_productId(product_id, n)
+    
+    recommendation = []
+    for rec in recommendations:
+        clicked = rec['clicked_product']
+        for product in enumerate(rec['recommended_products']):
+            recommendation.append({'id':product[1]['id'].item(), 'name':product[1]['name'], 'reason':f'You clicked on {clicked}', 'similarity_score':product[1]['similarity_score']})
+            
+    for rec in recommendation:
+        rec['id'] = int(rec['id'])
+
+    # Sort recommendations based on similarity_score in descending order
+    sorted_recommendations = sorted(recommendation, key=lambda x: x['similarity_score'], reverse=True)
+    
+    max_similarity = {}
+    for rec in sorted_recommendations:
+        id_ = rec['id']
+        score = rec['similarity_score']
+        if id_ not in max_similarity or score > max_similarity[id_]['similarity_score']:
+            max_similarity[id_] = rec
+
+    # Step 2: Extract values from the dictionary to get filtered recommendations
+    filtered_recommendations = list(max_similarity.values())
+
+    # Step 3: Print or use filtered_recommendations
+    print(filtered_recommendations)
+    return filtered_recommendations[:n]
+
--- recommendation_system.py
+++ recommendation_system.py
@@ -19,115 +19,112 @@ tfidf = TfidfVectorizer(stop_words = 'english')
 tfidf_matrix = tfidf.fit_transform(products['category'])
 cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
 
-indices = pd.Series(products.id.values, index=products['name'].drop_duplicates())
-cat = pd.Series(products['category'].values, index=products['name'].drop_duplicates())
-name = pd.Series(products['name'].values, index=products.index)
-level = pd.Series(products['level_of_study'].values, index=products.index)
+id = pd.Series(products['id'].values, index=products['name'].values)
+name = pd.Series(products['name'].values, index=products['id'].values)
+cat = pd.Series(products['category'].values, index=products['id'].values)
+level = pd.Series(products['level_of_study'].values, index=products['id'].values)
 
-def sameCategoryProducts(name, cosine_sim = cosine_sim):
-  index = indices[name]
-  sim_score = sorted(enumerate(cosine_sim[index]), key=lambda x:x[1], reverse=True)[0:20]
-  sim_index = [i[0] for i in sim_score]
-  sim_index_filtered = [i for i in sim_index if i != index]
-  return sim_score
+def sameCategoryProducts(id, cosine_sim = cosine_sim):
+    index = id
+    sim_score = sorted(enumerate(cosine_sim[index]), key=lambda x:x[1], reverse=True)[0:20]
+    return sim_score
 
 tfid = TfidfVectorizer(stop_words = 'english')
-tfid_matrix = tfidf.fit_transform(products['level_of_study'])
+tfid_matrix = tfid.fit_transform(products['level_of_study'])
 cosine_simf = linear_kernel(tfid_matrix, tfid_matrix)
 
-def sameLevelProducts(idx,cosine_simf = cosine_simf):
-  index = indices[idx]
-  simL_score = sorted(enumerate(cosine_simf[index]), key=lambda x:x[1], reverse=True)[0:20]
-  simL_index = [i[0] for i in simL_score]
-  simL_index_filtered = [i for i in simL_index if i != index]
-  return simL_score
+def sameLevelProducts(id,cosine_simf = cosine_simf):
+    index = id
+    simL_score = sorted(enumerate(cosine_simf[index]), key=lambda x:x[1], reverse=True)[0:20]
+    return simL_score
 
-# Create separate lists for prioritizing elements with the second element equal to 1.0
-@app.get("/recommend/{product}")
-def getRecommendation(product:str):
-  array1 = sameLevelProducts(product)
-  array2 = sameCategoryProducts(product)
-  
-  set1 = set(array1)
-  set2 = set(array2)
-  
-  # Identify common elements with second element value 1.0
-  common_elements = {item for item in set1 & set2 if item[1] == 1.0}
-  
-  # Separate lists
-  priority_common = []
-  priority_elements = []
-  other_elements = []
-  seen = set()
-  
-  # Function to add unique elements to respective lists
-  def add_unique_elements(array):
-      for item in array:
-          if item[0] not in seen:
-              if item in common_elements:
-                  priority_common.append(item)
-              elif item[1] == 1.0:
-                  priority_elements.append(item)
-              else:
-                  other_elements.append(item)
-              seen.add(item[0])
-  
-  # Add elements from both arrays
-  add_unique_elements(array1)
-  add_unique_elements(array2)
-  
-  # Combine the lists: common elements first, then other prioritized elements, then the rest
-  combined_array = priority_common + priority_elements + other_elements
-  
-  # Extract only the first elements
-  first_elements = [item[0] for item in combined_array if item[0]!=indices[product]]
-  
-  return name[first_elements]
-  array = [i[0] for i in combined_array]
-  return array
+# # Create separate lists for prioritizing elements with the second element equal to 1.0
+# @app.get("/recommend/{product}")
+# def getRecommendation(product:str):
+#     array1 = sameLevelProducts(product)
+#     array2 = sameCategoryProducts(product)
+    
+#     set1 = set(array1)
+#     set2 = set(array2)
+    
+#     # Identify common elements with second element value 1.0
+#     common_elements = {item for item in set1 & set2 if item[1] == 1.0}
+    
+#     # Separate lists
+#     priority_common = []
+#     priority_elements = []
+#     other_elements = []
+#     seen = set()
 
-@app.get("/recommendById/{id}")
+#     # Function to add unique elements to respective lists
+#     def add_unique_elements(array):
+#         for item in array:
+#             if item[0] not in seen:
+#                 if item in common_elements:
+#                     priority_common.append(item)
+#                 elif item[1] == 1.0:
+#                     priority_elements.append(item)
+#                 else:
+#                     other_elements.append(item)
+#                 seen.add(item[0])
+
+#     # Add elements from both arrays
+#     add_unique_elements(array1)
+#     add_unique_elements(array2)
+    
+#     # Combine the lists: common elements first, then other prioritized elements, then the rest
+#     combined_array = priority_common + priority_elements + other_elements
+#     # Extract only the first elements
+#     first_elements = [item[0] for item in combined_array if item[0]!=id[product]]
+    
+#     return name[first_elements]
+#     array = [i[0] for i in combined_array]
+#     return array
+
+@app.get("/recommendItems/{id}")
 def getRecommendationById(id: int):
-  product = name[id]
-  array1 = sameLevelProducts(product)
-  array2 = sameCategoryProducts(product)
-  
-  set1 = set(array1)
-  set2 = set(array2)
-  
-  # Identify common elements with second element value 1.0
-  common_elements = {item for item in set1 & set2 if item[1] == 1.0}
-  
-  # Separate lists
-  priority_common = []
-  priority_elements = []
-  other_elements = []
-  seen = set()
-  
-  # Function to add unique elements to respective lists
-  def add_unique_elements(array):
-      for item in array:
-          if item[0] not in seen:
-              if item in common_elements:
-                  priority_common.append(item)
-              elif item[1] == 1.0:
-                  priority_elements.append(item)
-              else:
-                  other_elements.append(item)
-              seen.add(item[0])
-  
-  # Add elements from both arrays
-  add_unique_elements(array1)
-  add_unique_elements(array2)
-  
-  # Combine the lists: common elements first, then other prioritized elements, then the rest
-  combined_array = priority_common + priority_elements + other_elements
-  # Extract only the first elements
-  first_elements = [item[0] for item in combined_array if item[0]!=id]
-  
-  return {"name":name[first_elements]}
-  array = [i[0] for i in combined_array]
-  return array
+    product = name[id]
+    array1 = sameLevelProducts(id)
+    array2 = sameCategoryProducts(id)
+    
+    set1 = set(array1)
+    set2 = set(array2)
+    
+    # Identify common elements with second element value 1.0
+    common_elements = {item for item in set1 & set2 if item[1] == 1.0}
+    
+    # Separate lists
+    priority_common = []
+    priority_elements = []
+    other_elements = []
+    seen = set()
+    
+    # Function to add unique elements to respective lists
+    def add_unique_elements(array, set):
+        for item in array:
+            if item[0] not in seen:
+                if item in common_elements:
+                    priority_common.append({"id":item[0], "reason":"Same category and level"})
+                elif item[1] == 1.0:
+                    if(set == 'level'):
+                        priority_elements.append({"id":item[0], "reason":"Same level"})
+                    elif(set == 'category'):
+                        priority_elements.append({"id":item[0], "reason":"Same category"})
+                else:
+                    other_elements.append({"id":item[0], "reason":"No reason."})
+                seen.add(item[0])
+    
+    # Add elements from both arrays
+    add_unique_elements(array2,'category')
+    add_unique_elements(array1,'level')
+    
+    # Combine the lists: common elements first, then other prioritized elements, then the rest
+    combined_array = priority_common + priority_elements + other_elements
+    combined_array = combined_array[:21]
+    # Extract only the first elements
+    recommendations = [item for item in combined_array if item['id']!=id]
+    
+    return [len(recommendations), recommendations]
 
-recommendation = getRecommendation('Laptop')
+recommendation = getRecommendationById(1)
 print(recommendation)
